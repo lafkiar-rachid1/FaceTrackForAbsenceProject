096d5bb0ec3967fde7b491c34e75cadd816e003c
lafkiar-rachid1 <rachidlafkiar@gmail.com>
2025-12-24 17:25:11 +0100

    vider le backend

--- backend/app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.models.user import User
-from app.models.attendance import Attendance
-
-__all__ = ["User", "Attendance"]
--- backend/app/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.schemas.user import UserCreate, UserResponse, UserLogin
-from app.schemas.attendance import AttendanceCreate, AttendanceResponse
-
-__all__ = ["UserCreate", "UserResponse", "UserLogin", "AttendanceCreate", "AttendanceResponse"]
